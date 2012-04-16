9987f0b59b750f8e4f88637d489cb7e1da99804a
Zach Smith <x.coder.zach@gmail.com>
2012-04-16 02:01:29 -0500

    implemented ObjectId

--- test/insert.js
+++ test/insert.js
@@ -1,11 +1,18 @@
 var db = require("../lib/database")
-
+  , ObjectId = require("../lib/object_id")
 
 describe("when I insert a document", function() {
   it("should be saved", function() {
     var things = db.collection("things")
+
     things.insert({ name: "thing" })
-    var coll = things.find({})
-    coll[0].name.should.equal("thing")
-  })  
+    things.find({})[0].name.should.equal("thing")
+  })
+  it("should generate an ObjectId for the inserted document", function() {
+    var things = db.collection("things")
+      , id = things.insert({ name: "thing" }) 
+
+    ;(id instanceof ObjectId).should.equal(true)
+    id.toString().length.should.equal(24)
+  })
 })
