71fc786b6e2be4a5590a4f5c7a635306c1c5ff1c
Zach Smith <x.coder.zach@gmail.com>
2012-04-16 01:23:17 -0500

    Added documentation for insert and find

--- test/insert.js
+++ test/insert.js
@@ -4,8 +4,8 @@ var db = require("../lib/database")
 describe("when I insert a document", function() {
   it("should be saved", function() {
     var things = db.collection("things")
-    things.insert({name: "thing"})
+    things.insert({ name: "thing" })
     var coll = things.find({})
     coll[0].name.should.equal("thing")
-  })
+  })  
 })
